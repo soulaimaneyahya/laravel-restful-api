e0aacbe1bf1191d5304c0cdcd2c7d362d59a3c1a
soulaimane yahya <soulaimaneyh07@gmail.com>
2022-10-31 10:23:24 +0100

    handling-exceptions

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,11 +2,16 @@
 
 namespace App\Exceptions;
 
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use App\Traits\ApiResponser;
 use Throwable;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 
 class Handler extends ExceptionHandler
 {
+    use ApiResponser;
     /**
      * A list of exception types with their corresponding custom log levels.
      *
@@ -43,8 +48,24 @@ class Handler extends ExceptionHandler
      */
     public function register()
     {
-        $this->reportable(function (Throwable $e) {
-            //
+        $this->renderable(function (NotFoundHttpException $e) {
+            return $this->infoResponse("Not Found", 404);
+        });
+
+        $this->renderable(function (AccessDeniedHttpException $e) {
+            return $this->infoResponse($e->getMessage(), 403);
+        });
+
+        $this->renderable(function (UnauthorizedHttpException $e) {
+            return $this->infoResponse($e->getMessage(), 403);
+        });
+
+        // other expected exceptions
+        $this->renderable(function (Throwable $e) {
+            if (config('app.debug')) {
+                return $this->infoResponse($e->getMessage(), 500);
+            }
+            return $this->infoResponse("Unexpected Exception. Try Later", 500);
         });
     }
 }
